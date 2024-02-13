cbbc8553efdf0fb219ff74ec77ae33013cf6d3cd
Jahir Noriega <64813299+jahirnoriega@users.noreply.github.com>
2024-02-12 19:46:00 -0600

    API UPDATED

--- app/models/maquillaje.server.js
+++ app/models/maquillaje.server.js
@@ -1,12 +1,12 @@
 export async function getMaquillaje() {
   // eslint-disable-next-line no-undef
-  const url = `http://localhost:1337/api/maquillajes?populate=*`;
+  const url = `https://backend-bagshop.onrender.com/api/maquillajes?populate=*`;
   const request = await fetch(url);
   const data = await request.json();
   return await data;
 }
 export async function getProducto(product) {
-  const url = `http://localhost:1337/api/maquillajes?filters[url]=${product}&populate=*`;
+  const url = `https://backend-bagshop.onrender.com/api/maquillajes?filters[url]=${product}&populate=*`;
   const request = await fetch(url);
   const data = await request.json();
   return await data;
